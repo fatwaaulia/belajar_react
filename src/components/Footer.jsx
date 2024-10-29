eb37cce5da53de1f6f3eb392f4fe2861a81ecd9d
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2024-10-29 07:53:05 +0700

    update: home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="container-fluid bg-dark text-white mt-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>Ini footer</h1>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
